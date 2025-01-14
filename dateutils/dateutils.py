9901a1ef81b4dff9c79e931e54dedf4dbdbfb74f
Chet Mancini <chetmancini@gmail.com>
2025-01-14 11:11:27 -0500

    cleanup imports

--- dateutils/dateutils.py
+++ dateutils/dateutils.py
@@ -1,7 +1,10 @@
 import calendar
 import time
 from wsgiref.handlers import format_date_time
-import datetime
+from datetime import timezone
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
 
 
 ##################
@@ -14,12 +17,12 @@ def utc_now_seconds() -> int:
     return calendar.timegm(time.gmtime())
 
 
-def utc_today() -> datetime.date:
-    return datetime.datetime.now(datetime.timezone.utc).date()
+def utc_today() -> date:
+    return datetime.now(timezone.utc).date()
 
 
 def utc_truncate_epoch_day(ts: int) -> int:
-    dt = datetime.datetime.fromtimestamp(ts, datetime.timezone.utc)
+    dt = datetime.fromtimestamp(ts, timezone.utc)
     dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
     return epoch_s(dt)
 
@@ -28,56 +31,52 @@ def utc_from_timestamp(ts: int):
     """
     Convert a timestamp to a datetime object in UTC timezone
     """
-    return datetime.datetime.fromtimestamp(ts, datetime.timezone.utc)
+    return datetime.fromtimestamp(ts, timezone.utc)
 
 
-def epoch_s(dt: datetime.datetime) -> int:
+def epoch_s(dt: datetime) -> int:
     """
     Convert a datetime object to a unix timestamp
     """
     return calendar.timegm(dt.utctimetuple())
 
 
-def datetime_start_of_day(day: datetime.date) -> datetime.datetime:
-    return datetime.datetime.combine(day, datetime.datetime.min.time())
+def datetime_start_of_day(day: date) -> datetime:
+    return datetime.combine(day, datetime.min.time())
 
 
-def datetime_end_of_day(day: datetime.date) -> datetime.datetime:
-    return (
-        datetime_start_of_day(day)
-        + datetime.timedelta(days=1)
-        - datetime.timedelta(seconds=1)
-    )
+def datetime_end_of_day(day: date) -> datetime:
+    return datetime_start_of_day(day) + timedelta(days=1) - timedelta(seconds=1)
 
 
 ##################
 # Quarter operations
 ##################
-def date_to_quarter(dt: datetime.date) -> int:
+def date_to_quarter(dt: date) -> int:
     return ((dt.month - 1) // 3) + 1
 
 
-def date_to_start_of_quarter(dt: datetime.date) -> datetime.date:
+def date_to_start_of_quarter(dt: date) -> date:
     new_month = (((dt.month - 1) // 3) * 3) + 1
     return dt.replace(day=1, month=new_month)
 
 
-def start_of_quarter(year: int, q: int) -> datetime.datetime:
+def start_of_quarter(year: int, q: int) -> datetime:
     """
     Get the start of the quarter
     """
-    return datetime.datetime(year, (q - 1) * 3 + 1, 1)
+    return datetime(year, (q - 1) * 3 + 1, 1)
 
 
 def end_of_quarter(year: int, q: int):
     """
     Get the end of the quarter
     """
-    return datetime.datetime(year, q * 3, 1, 23, 59, 59)
+    return datetime(year, q * 3, 1, 23, 59, 59)
 
 
 def generate_quarters(until_year=1970, until_q=1):
-    today = datetime.date.today()
+    today = date.today()
     current_quarter = date_to_quarter(today)
     current_year = today.year
     for year in generate_years(until=until_year):
@@ -95,16 +94,16 @@ def generate_quarters(until_year=1970, until_q=1):
 ##################
 
 
-def start_of_year(year: int) -> datetime.datetime:
-    return datetime.datetime(year, 1, 1)
+def start_of_year(year: int) -> datetime:
+    return datetime(year, 1, 1)
 
 
-def end_of_year(year: int) -> datetime.datetime:
-    return datetime.datetime(year, 12, 31, 23, 59, 59)
+def end_of_year(year: int) -> datetime:
+    return datetime(year, 12, 31, 23, 59, 59)
 
 
 def generate_years(until: int = 1970):
-    current_year = datetime.date.today().year
+    current_year = date.today().year
     for years_ago in range(current_year + 1 - until):
         yield current_year - years_ago
 
@@ -112,17 +111,17 @@ def generate_years(until: int = 1970):
 ##################
 # Month operations
 ##################
-def start_of_month(year: int, month: int) -> datetime.datetime:
-    return datetime.datetime(year, month, 1)
+def start_of_month(year: int, month: int) -> datetime:
+    return datetime(year, month, 1)
 
 
 def end_of_month(year, month):
     days_in_month = calendar.monthrange(year, month)[1]
-    return datetime.datetime(year, month, days_in_month, 23, 59, 59)
+    return datetime(year, month, days_in_month, 23, 59, 59)
 
 
 def generate_months(until_year=1970, until_m=1):
-    today = datetime.date.today()
+    today = date.today()
     for year in generate_years(until=until_year):
         for month in range(12, 0, -1):
             if year == today.year and month > today.month:
@@ -136,12 +135,12 @@ def generate_months(until_year=1970, until_m=1):
 ##################
 # Week operations
 ##################
-def generate_weeks(count: int = 500, until_date: datetime.date | None = None):
-    this_dow = datetime.date.today().weekday()
-    monday = datetime.date.today() - datetime.timedelta(days=this_dow)
+def generate_weeks(count: int = 500, until_date: date | None = None):
+    this_dow = date.today().weekday()
+    monday = date.today() - timedelta(days=this_dow)
     end = monday
     for i in range(count):
-        start = end - datetime.timedelta(days=7)
+        start = end - timedelta(days=7)
         ret = (start, end)
         end = start
         if until_date and start > until_date:
@@ -152,15 +151,11 @@ def generate_weeks(count: int = 500, until_date: datetime.date | None = None):
             return
 
 
-def _ts_difference(timestamp: int | datetime.datetime | None = None, now_override=None):
-    now = (
-        datetime.datetime.now()
-        if not now_override
-        else datetime.datetime.fromtimestamp(now_override)
-    )
+def _ts_difference(timestamp: int | datetime | None = None, now_override=None):
+    now = datetime.now() if not now_override else datetime.fromtimestamp(now_override)
     if type(timestamp) is int:
         diff = now - datetime.fromtimestamp(timestamp)
-    elif isinstance(timestamp, datetime.datetime):
+    elif isinstance(timestamp, datetime):
         diff = now - timestamp
     elif not time:
         diff = now - now
@@ -203,7 +198,7 @@ def pretty_date(timestamp=None, now_override=None):  # NOQA
     return str(day_diff / 365) + " years ago"
 
 
-def httpdate(date_time: datetime.datetime) -> str:
+def httpdate(date_time: datetime) -> str:
     """
     Convert a datetime object to an HTTP date string
     """
